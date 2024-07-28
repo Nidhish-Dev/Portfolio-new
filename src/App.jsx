81b520c76a3931d21f3ccef08bfbd193687383a5
Nidhish Rathore <codenidhish07@gmail.com>
2024-07-28 18:55:40 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Manager from './components/Manager'
+
+function App() {
+
+  return (
+    <>
+    <Manager />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Manager.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Manager = () => {
+  return (
+    
+    <div class="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+    
+  )
+}
+
+export default Manager
