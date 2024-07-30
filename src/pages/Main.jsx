c49f019ffb1b222c3de370e91d0c9dc47d64e1f9
Nidhish Rathore <codenidhish07@gmail.com>
2024-07-30 21:59:32 +0530

    home fixes

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,30 +1,31 @@
-import React from 'react'
-import Navbar from '../components/Navbar';
-import { Link } from 'react-router-dom'
-import './Pages.css'
+import React from "react";
+import Navbar from "../components/Navbar";
+import { Link } from "react-router-dom";
+import "./Pages.css";
 
 const main = () => {
   return (
     <>
-    <Navbar />
-    
-    <div className="main-container">
-      <div className="maintext">
-<p>Turning ideas into real</p>
-<p>life <span>products </span>is my</p>
-<p>calling.</p>
+      <Navbar />
 
-      </div>
-      
- <Link to="/projects">
-        <div className="viewProjectsBtn">
-          View Projects
+      <div className="main-container">
+        <div className="maintext">
+          <p>Turning ideas into real</p>
+          <p>
+            life <span>products </span>is my
+          </p>
+          <p>calling.</p>
         </div>
-      </Link>
+        <Link to="/projects">
+          <div className="viewProjectsBtn">View Projects</div>
+        </Link>
+      </div>
+      <div className="know-more " style={{ fontFamily: 'Montserrat, sans-serif' }}>
+        <p>Dive Into My Journey -
+        <Link to="/about">ABOUT</Link></p>
+      </div>
+    </>
+  );
+};
 
-    </div>
-  </>
-  )
-}
-
-export default main
+export default main;
