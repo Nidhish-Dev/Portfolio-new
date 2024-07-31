247320ada2378e08e67af322724782623bf1e1d2
Nidhish Rathore <codenidhish07@gmail.com>
2024-07-31 13:19:37 +0530

    projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -36,9 +36,179 @@ const Projects = () => {
           </button>
         </div>
       </div>
-      <div className="project-container">
-      Projects Will be added soon!
-      </div>
+
+
+<div className="project-container mt-12 mb-10 flex flex-wrap gap-4">
+{/* 1st */}
+
+<div className="relative flex flex-col mt-6 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-full sm:w-96">
+  <div
+    className="relative h-56 mx-4 -mt-6 overflow-hidden text-white shadow-lg bg-clip-border rounded-xl bg-blue-gray-500 shadow-blue-gray-500/40"
+  >
+    <img
+      src="/chatting.jpg"
+      alt="card-image"
+      className="w-full h-full object-cover"
+    />
+  </div>
+  <div className="p-4 sm:p-6">
+    <h5 className="block mb-2 font-sans text-lg sm:text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
+      Chatify
+    </h5>
+    <p className="block font-sans text-sm sm:text-base antialiased font-light leading-relaxed text-inherit">
+    The application is a chat group platform developed with React and Firebase, allowing users to engage in conversations with others using their names. It features Google authentication for seamless login and user management.</p>
+  </div>
+  <div className="p-4 sm:p-6 pt-0">
+    <a target='/' href="https://nidhish-dev.github.io/Chatify/"><button
+      className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs sm:text-sm py-2 sm:py-3 px-4 sm:px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
+      type="button"
+    >
+      Visit &gt;
+    </button></a>
+  </div>
+</div>
+
+{/* 2nd */}
+<div className="relative flex flex-col mt-6 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-full sm:w-96">
+  <div
+    className="relative h-56 mx-4 -mt-6 overflow-hidden text-white shadow-lg bg-clip-border rounded-xl bg-blue-gray-500 shadow-blue-gray-500/40"
+  >
+    <img
+      src="/moviesx.jpg"
+      alt="card-image"
+      className="w-full h-full object-cover"
+    />
+  </div>
+  <div className="p-4 sm:p-6">
+    <h5 className="block mb-2 font-sans text-lg sm:text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
+      MoviesX
+    </h5>
+    <p className="block font-sans text-sm sm:text-base antialiased font-light leading-relaxed text-inherit">
+    Built with React, this application allows users to search for movies and view their release years. It leverages API integration to fetch and display detailed movie information, enhancing user interaction and data accessibility.</p>
+  </div>
+  <div className="p-4 sm:p-6 pt-0">
+    <a target='/' href="https://nidhishr7.github.io/MoviesX/"><button
+      className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs sm:text-sm py-2 sm:py-3 px-4 sm:px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
+      type="button"
+    >
+      Visit &gt;
+    </button></a>
+  </div>
+</div>
+
+{/* 3rd */}
+<div className="relative flex flex-col mt-6 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-full sm:w-96">
+  <div
+    className="relative h-56 mx-4 -mt-6 overflow-hidden text-white shadow-lg bg-clip-border rounded-xl bg-blue-gray-500 shadow-blue-gray-500/40"
+  >
+    <img
+      src="/neeraj.jpg"
+      alt="card-image"
+      className="w-full h-full object-cover"
+    />
+  </div>
+  <div className="p-4 sm:p-6">
+    <h5 className="block mb-2 font-sans text-lg sm:text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
+      Neeraj's Portfolio
+    </h5>
+    <p className="block font-sans text-sm sm:text-base antialiased font-light leading-relaxed text-inherit">
+    This project focuses on frontend development with React. It serves as a portfolio, featuring an integrated Figma prototype to showcase design and interactive elements, demonstrating both visual and technical skills.</p>
+  </div>
+  <div className="p-4 sm:p-6 pt-0">
+    <a target='/' href="https://neeraj-design.github.io/Portfolio/"><button
+      className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs sm:text-sm py-2 sm:py-3 px-4 sm:px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
+      type="button"
+    >
+      Visit &gt;
+    </button></a>
+  </div>
+</div>
+
+{/* 4th */}
+
+<div className="relative flex flex-col mt-6 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-full sm:w-96">
+  <div
+    className="relative h-56 mx-4 -mt-6 overflow-hidden text-white shadow-lg bg-clip-border rounded-xl bg-blue-gray-500 shadow-blue-gray-500/40"
+  >
+    <img
+      src="/contactme.png"
+      alt="card-image"
+      className="w-full h-full object-cover"
+    />
+  </div>
+  <div className="p-4 sm:p-6">
+    <h5 className="block mb-2 font-sans text-lg sm:text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
+      Contact ME
+    </h5>
+    <p className="block font-sans text-sm sm:text-base antialiased font-light leading-relaxed text-inherit">
+    This webpage is a contact form developed with React, allowing users to enter their details. Submitted information is securely sent and stored in a Firebase database, ensuring efficient data management and retrieval.</p>
+  </div>
+  <div className="p-4 sm:p-6 pt-0">
+    <a target='/' href="https://nidhish-dev.github.io/Contact_me/"><button
+      className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs sm:text-sm py-2 sm:py-3 px-4 sm:px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
+      type="button"
+    >
+      Visit &gt;
+    </button></a>
+  </div>
+</div>
+
+{/* 5th */}
+<div className="relative flex flex-col mt-6 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-full sm:w-96">
+  <div
+    className="relative h-56 mx-4 -mt-6 overflow-hidden text-white shadow-lg bg-clip-border rounded-xl bg-blue-gray-500 shadow-blue-gray-500/40"
+  >
+    <img
+      src="/weather.jpg"
+      alt="card-image"
+      className="w-full h-full object-cover"
+    />
+  </div>
+  <div className="p-4 sm:p-6">
+    <h5 className="block mb-2 font-sans text-lg sm:text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
+      Weather
+    </h5>
+    <p className="block font-sans text-sm sm:text-base antialiased font-light leading-relaxed text-inherit">
+    This weather app, created using HTML, CSS, and JavaScript, fetches weather data via API. Users can search for weather conditions and view detailed information for numerous locations, providing comprehensive weather insights.</p>
+  </div>
+  <div className="p-4 sm:p-6 pt-0">
+    <a target='/' href="https://nidhish-dev.github.io/Weather/"><button
+      className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs sm:text-sm py-2 sm:py-3 px-4 sm:px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
+      type="button"
+    >
+      Visit &gt;
+    </button></a>
+  </div>
+</div>
+
+{/* 6th */}
+<div className="relative flex flex-col mt-6 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-full sm:w-96">
+  <div
+    className="relative h-56 mx-4 -mt-6 overflow-hidden text-white shadow-lg bg-clip-border rounded-xl bg-blue-gray-500 shadow-blue-gray-500/40"
+  >
+    <img
+      src="/microsoft.jpg"
+      alt="card-image"
+      className="w-full h-full object-cover"
+    />
+  </div>
+  <div className="p-4 sm:p-6">
+    <h5 className="block mb-2 font-sans text-lg sm:text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
+      Microsoft Clone
+    </h5>
+    <p className="block font-sans text-sm sm:text-base antialiased font-light leading-relaxed text-inherit">
+    I created a basic clone of the Microsoft website using HTML and CSS. This project replicates the site’s layout and design, demonstrating fundamental web development skills and proficiency in front-end technologies.</p>
+  </div>
+  <div className="p-4 sm:p-6 pt-0">
+    <a target='/' href="https://nidhish-dev.github.io/Microsoft_clone/"><button
+      className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs sm:text-sm py-2 sm:py-3 px-4 sm:px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
+      type="button"
+    >
+      Visit &gt;
+    </button></a>
+  </div>
+</div>
+ </div>
     </>
   )
 }
